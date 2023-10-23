a7f006c1fb7ee49072517fad16822b6ac53fbae8
Jean Carlos Almeida <jncaal89@gmail.com>
2023-10-23 15:36:18 -0300

    segundo commit

--- PROJETO - Estruturas e comandos de repetição .py
+++ PROJETO - Estruturas e comandos de repetição .py
@@ -1,7 +1,36 @@
-i = 0
-for i in range(20):
- i = i + 1
- if (i == 13):
+numero = 0
+for i in range(21):
+ numero = numero + 1
+ if (numero == 13):
   continue
-while (i <= 20):
-  print(i)
+ if (numero == 21):
+  break
+ print(numero)
+
+numero = 21
+for i in range(21,0,-1):
+ numero = numero - 1
+ if (numero == 13):
+  continue
+ if (numero == 0):
+  break
+ print(numero)
+
+
+i = 0
+while (i <= 21):
+    i = i + 1
+    if (i == 13):
+        continue
+    if (i == 21):
+        break
+    print(i)
+
+i = 21
+while (i >= 0):
+    i = i - 1
+    if (i == 13):
+        continue
+    if (i == 0):
+        break
+    print(i) 
