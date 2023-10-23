0fca33e31be9f31524835d9b4da53e2e8562923d
Jean Carlos Almeida <jncaal89@gmail.com>
2023-10-23 14:40:30 -0300

    primeiro commit

---
+++ Aula.py
@@ -0,0 +1,7 @@
+nota = 4
+if (nota < 4):
+   print("Reprovado")
+elif (nota < 7):
+   print("Em recuperação")
+elif(nota >=7):
+   print("Aprovado")
---
+++ PROJETO - Estruturas e comandos de repetição .py
@@ -0,0 +1,7 @@
+i = 0
+for i in range(20):
+ i = i + 1
+ if (i == 13):
+  continue
+while (i <= 20):
+  print(i)
